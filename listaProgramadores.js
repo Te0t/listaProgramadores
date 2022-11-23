9d3ace1d173751290f2f351af73e19a6e9c6db29
206 <206@academicos.cesde.edu.co>
2022-11-23 16:42:44 -0500

    se organiza el codigo

--- listaProgramadores.js
+++ listaProgramadores.js
@@ -1,11 +1,10 @@
 const listaProgramadores = ["Gorka", "Martin", "Alex", "Leire"]
 
-let nombres = []
-
 //Programacion imperactiva
+let nombres = []
 listaProgramadores.forEach((programador, posicion) => {
     nombres[posicion] = programador
 })
 
 // Programacion declarativa
-nombres = [...listaProgramadores]
+let nombres = [...listaProgramadores]
